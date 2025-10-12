16e42227ebc8e2c3afc6409a141cb4386bfd2db0
ippapi <coricutiphong@gmail.com>
2025-10-12 21:13:31 +0700

    Rolling in the space to fall into your skin

--- corruptor/main.py
+++ corruptor/main.py
@@ -1,14 +1,13 @@
 import torch
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
+import os
 import time
 import argparse
 import random
 from tqdm import tqdm
 import logging
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+import sys
 from corruptor.utils.train import train
 from corruptor.utils.evaluate import evaluate
 from corruptor.utils.generate import predict
